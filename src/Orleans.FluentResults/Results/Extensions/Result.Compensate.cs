d889a8d04395e9f0c27aef90bf1ddfb166489a84
Leo Hong <hongliyu2002@hotmail.com>
2023-03-01 21:40:53 +0800

    Update

---
+++ src/Orleans.FluentResults/Results/Extensions/Result.Compensate.cs
@@ -0,0 +1,117 @@
+﻿namespace Orleans.FluentResults;
+
+/// <summary>
+/// </summary>
+public static partial class ResultExtensions
+{
+
+    #region Compensate
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="compensate"></param>
+    public static Result Compensate(this Result result, Func<IEnumerable<IError>, Result> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        return result.IsSuccess ? result : compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Full Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="compensate"></param>
+    public static async Task<Result> CompensateAsync(this Task<Result> resultTask, Func<IEnumerable<IError>, Task<Result>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess ? result : await compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Full ValueTask Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="compensate"></param>
+    public static async ValueTask<Result> CompensateAsync(this ValueTask<Result> resultTask, Func<IEnumerable<IError>, ValueTask<Result>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess ? result : await compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Right Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="compensate"></param>
+    public static async Task<Result> CompensateAsync(this Result result, Func<IEnumerable<IError>, Task<Result>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        return result.IsSuccess ? result : await compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Right ValueTask Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="compensate"></param>
+    public static async ValueTask<Result> CompensateAsync(this Result result, Func<IEnumerable<IError>, ValueTask<Result>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        return result.IsSuccess ? result : await compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Left Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="compensate"></param>
+    public static async Task<Result> CompensateAsync(this Task<Result> resultTask, Func<IEnumerable<IError>, Result> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess ? result : compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Left ValueTask Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="compensate"></param>
+    public static async ValueTask<Result> CompensateAsync(this ValueTask<Result> resultTask, Func<IEnumerable<IError>, Result> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess ? result : compensate(result.Errors);
+    }
+
+    #endregion
+
+}
---
+++ src/Orleans.FluentResults/Results/Extensions/ResultTValue.Compensate.cs
@@ -0,0 +1,117 @@
+﻿namespace Orleans.FluentResults;
+
+/// <summary>
+/// </summary>
+public static partial class ResultTExtensions
+{
+
+    #region Compensate
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="compensate"></param>
+    public static Result<T> Compensate<T>(this Result<T> result, Func<IEnumerable<IError>, Result<T>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        return result.IsSuccess ? result : compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Full Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="compensate"></param>
+    public static async Task<Result<T>> CompensateAsync<T>(this Task<Result<T>> resultTask, Func<IEnumerable<IError>, Task<Result<T>>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess ? result : await compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Full ValueTask Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="compensate"></param>
+    public static async ValueTask<Result<T>> CompensateAsync<T>(this ValueTask<Result<T>> resultTask, Func<IEnumerable<IError>, ValueTask<Result<T>>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess ? result : await compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Right Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="compensate"></param>
+    public static async Task<Result<T>> CompensateAsync<T>(this Result<T> result, Func<IEnumerable<IError>, Task<Result<T>>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        return result.IsSuccess ? result : await compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Right ValueTask Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="compensate"></param>
+    public static async ValueTask<Result<T>> CompensateAsync<T>(this Result<T> result, Func<IEnumerable<IError>, ValueTask<Result<T>>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        return result.IsSuccess ? result : await compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Left Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="compensate"></param>
+    public static async Task<Result<T>> CompensateAsync<T>(this Task<Result<T>> resultTask, Func<IEnumerable<IError>, Result<T>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess ? result : compensate(result.Errors);
+    }
+
+    #endregion
+
+    #region Compensate Left ValueTask Async
+
+    /// <summary>
+    ///     If the calling result is a failure, the given compensate function is executed and its result is returned.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="compensate"></param>
+    public static async ValueTask<Result<T>> CompensateAsync<T>(this ValueTask<Result<T>> resultTask, Func<IEnumerable<IError>, Result<T>> compensate)
+    {
+        ArgumentNullException.ThrowIfNull(compensate);
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess ? result : compensate(result.Errors);
+    }
+
+    #endregion
+
+}
