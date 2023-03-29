8c1495d3a563e1ada55d738cc7c37b099567edae
Leo Hong <hongliyu2002@hotmail.com>
2023-03-29 17:52:24 +0800

    Update

---
+++ src/Orleans.FluentResults/Results/Extensions/Result.MapIf.Unit.Predicate.cs
@@ -0,0 +1,130 @@
+﻿namespace Orleans.FluentResults;
+
+/// <summary>
+/// </summary>
+public static partial class ResultExtensions
+{
+
+    #region MapIf
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    public static Result MapIf(this Result result, Func<bool> predicate, Action map)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        return predicate() ? result.Map(map) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapIf Full Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    public static async Task<Result> MapIfAsync(this Task<Result> resultTask, Func<bool> predicate, Func<Task> map, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        var result = await resultTask.ConfigureAwait(configureAwait);
+        return predicate() ? await result.MapAsync(map, configureAwait).ConfigureAwait(configureAwait) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapIf Full ValueTask Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    public static async ValueTask<Result> MapIfAsync(this ValueTask<Result> resultTask, Func<bool> predicate, Func<ValueTask> map, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        var result = await resultTask.ConfigureAwait(configureAwait);
+        return predicate() ? await result.MapAsync(map, configureAwait).ConfigureAwait(configureAwait) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapIf Right Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    public static async Task<Result> MapIfAsync(this Result result, Func<bool> predicate, Func<Task> map, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        return predicate() ? await result.MapAsync(map, configureAwait).ConfigureAwait(configureAwait) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapIf Right ValueTask Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    public static async ValueTask<Result> MapIfAsync(this Result result, Func<bool> predicate, Func<ValueTask> map, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        return predicate() ? await result.MapAsync(map, configureAwait).ConfigureAwait(configureAwait) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapIf Left Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    public static async Task<Result> MapIfAsync(this Task<Result> resultTask, Func<bool> predicate, Action map, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        var result = await resultTask.ConfigureAwait(configureAwait);
+        return predicate() ? result.Map(map) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapIf Left ValueTask Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    public static async ValueTask<Result> MapIfAsync(this ValueTask<Result> resultTask, Func<bool> predicate, Action map, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        var result = await resultTask.ConfigureAwait(configureAwait);
+        return predicate() ? result.Map(map) : result.ToResult();
+    }
+
+    #endregion
+
+}
---
+++ src/Orleans.FluentResults/Results/Extensions/Result.MapTryIf.Unit.Predicate.cs
@@ -0,0 +1,137 @@
+﻿namespace Orleans.FluentResults;
+
+/// <summary>
+/// </summary>
+public static partial class ResultExtensions
+{
+
+    #region MapTryIf
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="catchHandler"></param>
+    public static Result MapTryIf(this Result result, Func<bool> predicate, Action map, Func<Exception, IError>? catchHandler = null)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        return predicate() ? result.MapTry(map, catchHandler) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapTryIf Full Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    /// <param name="catchHandler"></param>
+    public static async Task<Result> MapTryIfAsync(this Task<Result> resultTask, Func<bool> predicate, Func<Task> map, Func<Exception, IError>? catchHandler = null, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        var result = await resultTask.ConfigureAwait(configureAwait);
+        return predicate() ? await result.MapTryAsync(map, catchHandler, configureAwait).ConfigureAwait(configureAwait) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapTryIf Full ValueTask Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    /// <param name="catchHandler"></param>
+    public static async ValueTask<Result> MapTryIfAsync(this ValueTask<Result> resultTask, Func<bool> predicate, Func<ValueTask> map, Func<Exception, IError>? catchHandler = null, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        var result = await resultTask.ConfigureAwait(configureAwait);
+        return predicate() ? await result.MapTryAsync(map, catchHandler, configureAwait).ConfigureAwait(configureAwait) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapTryIf Right Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    /// <param name="catchHandler"></param>
+    public static async Task<Result> MapTryIfAsync(this Result result, Func<bool> predicate, Func<Task> map, Func<Exception, IError>? catchHandler = null, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        return predicate() ? await result.MapTryAsync(map, catchHandler, configureAwait).ConfigureAwait(configureAwait) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapTryIf Right ValueTask Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    /// <param name="catchHandler"></param>
+    public static async ValueTask<Result> MapTryIfAsync(this Result result, Func<bool> predicate, Func<ValueTask> map, Func<Exception, IError>? catchHandler = null, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        return predicate() ? await result.MapTryAsync(map, catchHandler, configureAwait).ConfigureAwait(configureAwait) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapTryIf Left Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    /// <param name="catchHandler"></param>
+    public static async Task<Result> MapTryIfAsync(this Task<Result> resultTask, Func<bool> predicate, Action map, Func<Exception, IError>? catchHandler = null, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        var result = await resultTask.ConfigureAwait(configureAwait);
+        return predicate() ? result.MapTry(map, catchHandler) : result.ToResult();
+    }
+
+    #endregion
+
+    #region MapTryIf Left ValueTask Async
+
+    /// <summary>
+    ///     Execute an map function which returns a <see cref="Result{TOutput}" />.
+    /// </summary>
+    /// <param name="resultTask"></param>
+    /// <param name="predicate"></param>
+    /// <param name="map">map function</param>
+    /// <param name="configureAwait"></param>
+    /// <param name="catchHandler"></param>
+    public static async ValueTask<Result> MapTryIfAsync(this ValueTask<Result> resultTask, Func<bool> predicate, Action map, Func<Exception, IError>? catchHandler = null, bool configureAwait = true)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        var result = await resultTask.ConfigureAwait(configureAwait);
+        return predicate() ? result.MapTry(map, catchHandler) : result.ToResult();
+    }
+
+    #endregion
+
+}
